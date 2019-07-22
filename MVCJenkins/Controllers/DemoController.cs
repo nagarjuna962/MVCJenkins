63cc8ae46bffbcc41364a0579b57b84d2bb1a207
Kiran <kiran.noupada@outlook.com>
2019-07-22 16:13:13 +0530

    Add project files.

---
+++ MVCJenkins/Controllers/DemoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity.Core.Objects;
+using MVCJenkins.Models;
+
+namespace MVCJenkins.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        SampleEntities db = new SampleEntities();
+        public ActionResult ViewEmployees()
+        {
+            List<sp_viewemployee_Result> list = new List<sp_viewemployee_Result>();
+            list = db.sp_viewemployee().ToList();
+            return View(list);
+        }
+    }
+}
